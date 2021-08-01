e15e9333e1c77ab5658ae91d6a74f94b8d0ad444
Himanshu Choudhary <22himanshu14@gmail.com>
2021-08-01 10:34:22 +0530

    First Commit

---
+++ app/src/main/java/com/example1/iitinder/login/activities/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.example1.iitinder.login.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example1.iitinder.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
---
+++ app/src/main/java/com/example1/iitinder/signup/activities/SignupActivity.java
@@ -0,0 +1,19 @@
+package com.example1.iitinder.signup.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example1.iitinder.R;
+
+public class SignupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+
+        // TODO : Add fragments for signup
+
+    }
+}
